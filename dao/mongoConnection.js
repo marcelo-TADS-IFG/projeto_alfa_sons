b09c9cb738d50cbf22e149ed49cbee0cf1c4a77c
Marcelo Jesus <j.marcelo@estudantes.ifg.edu.br>
2025-10-25 17:42:50 -0300

    Tentando utilizar o banco no render

--- dao/mongoConnection.js
+++ dao/mongoConnection.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+/*require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
 const url = process.env.MONGODB_URI;
@@ -9,6 +9,34 @@ let db = null;
 async function connect() {
     if (db) return db;
     console.log("📡 URL recebida:", url); // <-- adicione esta linha temporária
+    const client = new MongoClient(url);
+    await client.connect();
+    console.log("🔗 Conectado a:", client.s.url);
+    db = client.db(dbName);
+    console.log(`✅ Conectado ao banco ${dbName}`);
+    return db;
+}
+
+module.exports = { connect };*/
+
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const url = process.env.MONGO_URI;
+const dbName = process.env.DB_NAME;
+
+console.log("Todas as variáveis de ambiente:");
+console.log("MONGO_URI:", process.env.MONGO_URI);
+console.log("DB_NAME:", process.env.DB_NAME);
+
+let db = null;
+
+async function connect() {
+    if (db) return db;
+    if (!url) {
+        console.error("❌ MONGO_URI não está definida!");
+        process.exit(1); // interrompe o app
+    }
     const client = new MongoClient(url);
     await client.connect();
     console.log("🔗 Conectado a:", client.s.url);
